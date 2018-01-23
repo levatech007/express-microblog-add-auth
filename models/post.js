a116e381ffe32c5f08368b7ea7cd5673195aaf0a
Eva <evaliisagalen@gmail.com>
2018-01-22 16:38:18 -0800

    added reference to user in post schema

--- models/post.js
+++ models/post.js
@@ -1,9 +1,11 @@
 var mongoose = require("mongoose"),
-  Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
+    User = require('./user');
 
 var PostSchema = new Schema({
   title: String,
   description: String,
+  user: {type: Schema.Types.ObjectId, ref:'User'},
 });
 
 var Post = mongoose.model("Post", PostSchema);
